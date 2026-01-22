3e4c78f947cc02076d131b3536ff3a2043b33759
AslamAsh <93033181+AslamAsh@users.noreply.github.com>
2026-01-22 16:01:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 function makeid(l) {
   // write your code here
+let chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+let str="";
+for(let i=0;i<l;i++)
+{
+    str+=chars[Math.floor(Math.random()*chars.length)];
+}
+	return str;
 }
 
 // Do not change the code below.
